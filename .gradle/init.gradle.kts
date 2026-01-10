feaf767f1a547d78e67f1eb86a3296bc7181e438
bigfang <bitair@gmail.com>
2026-01-11 01:10:24 +0800

    add ~/.gradle/init.gradle.kts

---
+++ .gradle/init.gradle.kts
@@ -0,0 +1,10 @@
+allprojects {
+    repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/public/") }
+        maven { url = uri("https://maven.aliyun.com/repository/google/") }
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin/") }
+        google()
+        mavenCentral()
+    }
+}
+
